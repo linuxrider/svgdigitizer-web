81e78cbed57217cfdcd5f3b26e4e86c671889dd6
Johannes Hermann <linuxrider@web.de>
2025-02-24 21:53:45 +0100

    add yaml template and validation against metadata-schema repo

--- src/components/YamlEdit.vue
+++ src/components/YamlEdit.vue
@@ -1,6 +1,7 @@
 <template>
-  <!-- CodeMirror Editor Container -->
-  <div ref="editorContainer" class="editor"></div>
+  <div class="container">
+    <div ref="editorContainer" class="editor"></div>
+  </div>
 </template>
 
 <script>
@@ -14,12 +15,12 @@ import { indentWithTab } from '@codemirror/commands'
 import { lintGutter } from '@codemirror/lint'
 import { yaml } from '@codemirror/lang-yaml'
 import { yamlSchema } from 'codemirror-json-schema/yaml'
-
+import { load as yamlLoad, dump as yamlDump } from 'js-yaml'
 import { usePublishStore } from '@/stores/publish'
 
-// import $RefParser from '@apidevtools/json-schema-ref-parser'
-// import { Buffer } from 'buffer'
-// globalThis.Buffer = Buffer
+import $RefParser from '@apidevtools/json-schema-ref-parser'
+import { Buffer } from 'buffer'
+globalThis.Buffer = Buffer
 
 export default {
   name: 'YamlEditor',
@@ -27,12 +28,13 @@ export default {
     const publishStore = usePublishStore()
     const editorContainer = ref(null)
     const errors = ref([])
-    const editorView = null
+    let editorView = null
 
     const initializeEditor = (schema) => {
       const state = EditorState.create({
         doc: '',
         extensions: [
+          EditorView.lineWrapping,
           EditorView.updateListener.of((v) => {
             if (v.docChanged) {
               publishStore.setYamlContent(v.state.doc.toString())
@@ -47,29 +49,45 @@ export default {
         ],
       })
 
-      new EditorView({
+      editorView = new EditorView({
         state,
         parent: editorContainer.value,
       })
+      console.log(editorContainer.value.offsetWidth)
+      // editorView.setSize(editorContainer.value.offsetWidth + 'px', 'auto')
+      populateTemplate()
+    }
+
+    const populateTemplate = async () => {
+      const response = await await fetch('/template.yaml')
+      const yaml = await response.text()
+
+      const curator = JSON.parse(localStorage.getItem('curator'))
+
+      try {
+        const doc = yamlLoad(yaml, 'utf8')
+
+        for (const key in curator) {
+          doc.curation.process[0][key] = curator[key]
+        }
+        doc.curation.process[0]['date'] = new Date().toISOString().substring(0, 10)
+
+        editorView.dispatch({
+          changes: { from: 0, to: editorView.state.doc.length, insert: yamlDump(doc, 'utf8') },
+        })
+      } catch (e) {
+        console.log(e)
+      }
     }
 
     onMounted(async () => {
-      const schema = await await (await fetch('/dereferenced-schema.json')).json()
-      // console.log(schema)
-      // await $RefParser.dereference(schema)
-      // await $RefParser.dereference(
-      //   '/metadata-schema/main/schemas/svgdigitizer.json',
-      // )
-      // const schema = await $RefParser.dereference('/metadata-schema/schemas/svgdigitizer.json', {
-      //   dereference: { circular: false },
-      // })
-      // console.log(JSON.stringify(schema, null, 2))
-      // console.log(schema)
-      // const parser = new $RefParser()
-      // parser.dereference(
-      //   '/metadata-schema/main/schemas/svgdigitizer.json',
-      // )
-      // console.log(parser.schema)
+      const schema = await $RefParser.dereference(
+        'https://raw.githubusercontent.com/echemdb/metadata-schema/main/schemas/svgdigitizer.json',
+        {
+          dereference: { circular: false },
+          // basepath: '/schemas',
+        },
+      )
       initializeEditor(schema)
     })
 
@@ -82,13 +100,22 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+}
 .editor {
   border: 1px solid #ccc;
   height: 65vh;
+  /* max-width: 98%; */
   width: 98%;
   margin-bottom: 20px;
+  overflow-y: scroll;
 }
-
+/* .CodeMirror {
+  width: 100% !important;
+  max-width: 600px;
+  white-space: pre;
+} */
 .error-list {
   margin-top: 10px;
   color: red;
@@ -110,5 +137,8 @@ export default {
   height: 4px;
   width: 100%;
   margin: 2px;
+}
+.cm-editor {
+  max-height: 10px;
 }
 </style>
